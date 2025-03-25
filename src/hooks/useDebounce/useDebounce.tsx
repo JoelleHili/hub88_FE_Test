fb9315d0c3bda4c1d330f040c2afaf44c0e266e7
Joelle Hili <joelle.hili@outlook.com>
2025-03-25 08:44:24 +0100

    chore: Updated Format

---
+++ src/hooks/useDebounce/useDebounce.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react"
+
+interface IUseDebounce {
+    debounceVariable: string | number
+    delay: number
+}
+
+const useDebounce: React.FC<IUseDebounce> = ({debounceVariable, delay = 300}) => {
+    
+
+    return <></>
+}
+
+export default useDebounce
---
+++ src/hooks/useDebounceFunction/useDebounceFunction.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react"
+
+interface IUseDebounceFunction {
+    debounceFuntion: () => void
+    delay: number
+}
+
+const useDebounceFunction: React.FC<IUseDebounceFunction> = ({debounceFuntion, delay = 300}) => {
+    
+
+    return <></>
+}
+
+export default useDebounceFunction
