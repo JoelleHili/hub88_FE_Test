ec2ff8241853dbfbb25ee918ac66cefea39431f7
Joelle Hili <joelle.hili@outlook.com>
2025-03-26 12:39:50 +0100

    feat: Added Test Structure and Jest

--- src/components/Table/components/table.tsx
+++ src/components/Table/components/table.tsx
@@ -3,29 +3,28 @@ interface ITable<T extends Record<string, string | number | boolean>> {
 }
 
 const table = <T extends Record<string, string | number | boolean>,>({ data }: ITable<T>) => {
-    if (data.length === 0) return <p className="table__empty">No data available</p>
+    return (<>
+        {data.length > 0 ?
+            <div className="table">
+                <div className="table__header__row">
+                    {Object.keys(data[0]).map((key) => (
+                        <div key={key} className="table__header">{key}</div>
+                    ))}
+                </div>
+                <div className="table__body">
+                    {data.map((row, rowIndex) => (
+                        <div className="table__row__container">
+                            <div className="table__row" key={rowIndex}>
 
-    return (
-        <div className="table">
-            <div className="table__header__row">
-                {Object.keys(data[0]).map((key) => (
-                    <div key={key} className="table__header">{key}</div>
-                ))}
-            </div>
-            <div className="table__body">
-                {data.map((row, rowIndex) => (
-                    <div className="table__row__container">
-                        <div className="table__row" key={rowIndex}>
-
-                            {Object.entries(row).map(([key, value]) => (
-                                <div key={key} className="table__cell">{String(value)}</div>
-                            ))}
+                                {Object.entries(row).map(([key, value]) => (
+                                    <div key={key} className="table__cell">{String(value)}</div>
+                                ))}
+                            </div>
+                            {rowIndex !== data.length - 1 && <div className="table__divider" />}
                         </div>
-                        {rowIndex !== data.length - 1 && <div className="table__divider" />}
-                    </div>
-                ))}
-            </div>
-        </div>
+                    ))}
+                </div>
+            </div> : <p className="table__empty">No data available</p>}</>
     )
 }
 
